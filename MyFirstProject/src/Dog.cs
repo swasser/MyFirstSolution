d4808ab94001641893a3e2b0c3a3ae054fa42ee5
Stas Wasserman <Stas.Wasserman@finning.com>
2019-07-10 08:56:03 -0700

    Add project files.

---
+++ MyFirstProject/src/Dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyFirstProject
+{
+    public class Dog
+    {
+        private int weight;
+        private string color;
+
+        public string breed;
+        public string name;
+
+        public Dog()
+        {
+            this.color = "black";
+            this.breed = "German Sheppard";
+            this.name = "Bob";
+        }
+
+        //Don't do this - StackOverflow
+        //Dog Bobby = new Dog();
+    }
+}
---
+++ MyFirstProject/test/DogTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstProject.test
+{
+    public class DogTests
+    {
+        [Test]
+        public void Dog_has_a_name()
+        {
+            Dog Sparky = new Dog();
+
+            Assert.That(Sparky.name.Length == 0, "Expect a dog to have a name, longer than 1 char");
+        }
+    }
+}
